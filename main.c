d16f8669d9d38c84be7876c91951d4c43ee29ff8
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-11-24 09:51:40 -0700

    Use vector types to store data since we will need to do some matrix/vector operations later

--- main.c
+++ main.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include "cvis/vis.h"
+#include "cmat/vec3d.h"
 
-double initial_position[3] = {
+Vec3d initial_position = {
     39.750661009097016,
     -104.99540760820614,
     1609.34
 };
 
-double initial_velocity[3] = {
+Vec3d initial_velocity = {
   0,
   0,
   0
 };
 
-double iniital_orientation[3] = {
+Vec3d iniital_orientation = {
     0,
     0,
     0
 };
 
+typedef struct ImuErrorModel {
+
+} ImuErrorModel;
+
+
 void DisplayConfigurationWindow() {
   if (igBegin("Configuration", NULL, ImGuiWindowFlags_None)) {
 
     if (igCollapsingHeader_TreeNodeFlags("Initial Starting Position", ImGuiTreeNodeFlags_None)) {
-      igInputDouble("Latitude  (deg)", &initial_position[0], -0.1, 10, "%.9f", ImGuiInputTextFlags_None);
-      igInputDouble("Longitude (deg)", &initial_position[1], -0.1, 10, "%.9f", ImGuiInputTextFlags_None);
-      igInputDouble("Height    (m)", &initial_position[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("Latitude  (deg)", &initial_position.vec[0], -0.1, 10, "%.9f", ImGuiInputTextFlags_None);
+      igInputDouble("Longitude (deg)", &initial_position.vec[1], -0.1, 10, "%.9f", ImGuiInputTextFlags_None);
+      igInputDouble("Height    (m)", &initial_position.vec[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
     }
     if (igCollapsingHeader_TreeNodeFlags("Initial Starting Velocity", ImGuiTreeNodeFlags_None)) {
-      igInputDouble("North (m/s)", &initial_velocity[0], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
-      igInputDouble("East  (m/s)", &initial_velocity[1], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
-      igInputDouble("Down  (m/s)", &initial_velocity[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("North (m/s)", &initial_velocity.vec[0], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("East  (m/s)", &initial_velocity.vec[1], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("Down  (m/s)", &initial_velocity.vec[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
     }
     if (igCollapsingHeader_TreeNodeFlags("Initial Starting Orientation", ImGuiTreeNodeFlags_None)) {
-      igInputDouble("Roll  (deg)", &iniital_orientation[0], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
-      igInputDouble("Pitch (deg)", &iniital_orientation[1], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
-      igInputDouble("Yaw   (deg)", &iniital_orientation[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("Roll  (deg)", &iniital_orientation.vec[0], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("Pitch (deg)", &iniital_orientation.vec[1], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
+      igInputDouble("Yaw   (deg)", &iniital_orientation.vec[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
     }
   }
   igEnd();
