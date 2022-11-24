c89f955d720339a35b64b891a41cab2555bf6f7d
Adam The Canadian <adam.jarrett.clare@gmail.com>
2022-11-24 14:07:33 -0700

    Add configuration of accelerometer and gyro biases

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "cvis/vis.h"
-#include "cmat/vec3d.h"
+#include "cmat/vec.h"
+#include "cmat/mat3d.h"
 
 Vec3d initial_position = {
     39.750661009097016,
@@ -21,9 +22,25 @@ Vec3d iniital_orientation = {
 };
 
 typedef struct ImuErrorModel {
+  Vec3d accelerometer_biases;
+  Vec3d gyro_biases;
 
+  /* Accelerometer scale factor and cross coupling */
+  Mat3d accelerometer_m;
+  /* Gyroscope scale factor and cross coupling */
+  Mat3d gyro_m;
+  /* Gyroscope g dependent biases */
+  Mat3d gyro_g;
+
+  double accelerometer_noise;
+  double gyro_noise;
+  double accelerometer_quantization;
+  double gyro_quantization;
 } ImuErrorModel;
 
+static struct ImuErrorModel imu_error_model;
+
+static void InitializeImuModel();
 
 void DisplayConfigurationWindow() {
   if (igBegin("Configuration", NULL, ImGuiWindowFlags_None)) {
@@ -43,12 +60,29 @@ void DisplayConfigurationWindow() {
       igInputDouble("Pitch (deg)", &iniital_orientation.vec[1], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
       igInputDouble("Yaw   (deg)", &iniital_orientation.vec[2], -0.1, 10, "%.3f", ImGuiInputTextFlags_None);
     }
+
+    if (igCollapsingHeader_TreeNodeFlags("IMU Model", ImGuiTreeNodeFlags_None)) {
+      /* Imgui really only has support for floats so we will convert our imu model parameters to floats for
+       * user inputs and then convert back. Annoying but only option for now */
+      static Vec3f tmp;
+      tmp = Vec3d_ToVec3f(&imu_error_model.accelerometer_biases);
+      if (igInputFloat3("Accelerometer Biases", &tmp.vec[0], "%.3f", ImGuiInputTextFlags_None)) {
+        imu_error_model.accelerometer_biases = Vec3f_ToVec3d(&tmp);
+      }
+
+      tmp = Vec3d_ToVec3f(&imu_error_model.gyro_biases);
+      if (igInputFloat3("Gyro Biases", &tmp.vec[0], "%.3f", ImGuiInputTextFlags_None)) {
+        imu_error_model.gyro_biases = Vec3f_ToVec3d(&tmp);
+      }
+    }
   }
   igEnd();
 }
 
 int main(int argc, char *argv[]) {
   visWindow_Initialize("GNSS INS", 800, 800);
+
+  InitializeImuModel();
 
   while (!visWindow_ShouldClose()) {
     visWindow_NewFrame();
@@ -57,4 +91,18 @@ int main(int argc, char *argv[]) {
 
     visWindow_EndFrame();
   }
+}
+
+void InitializeImuModel() {
+  Vec3d_SetZero(&imu_error_model.accelerometer_biases);
+  Vec3d_SetZero(&imu_error_model.gyro_biases);
+
+  Mat3d_SetZero(&imu_error_model.accelerometer_m);
+  Mat3d_SetZero(&imu_error_model.gyro_m);
+  Mat3d_SetZero(&imu_error_model.gyro_g);
+
+  imu_error_model.accelerometer_noise = 0;
+  imu_error_model.gyro_noise = 0;
+  imu_error_model.accelerometer_quantization = 0;
+  imu_error_model.gyro_quantization = 0;
 }
